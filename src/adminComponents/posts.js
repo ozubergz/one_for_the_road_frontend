b14e1ceb5d75db3d3c2148fdf40770d2354b4625
ozubergz <jmin499@gmail.com>
2020-02-27 15:04:24 -0500

    add items field

--- src/adminComponents/posts.js
+++ src/adminComponents/posts.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import { List, Datagrid, TextField } from 'react-admin';
+import { List, Datagrid, TextField, ArrayField, SingleFieldList, ChipField } from 'react-admin';
 
 export const PostList = (props) => (
     <List {...props}>
         <Datagrid>
             <TextField source="id" />
-            <TextField source="title" />
-            {/* <TextField source="body" /> */}
+            <TextField source="name" />
+            <ArrayField source="items">
+                <SingleFieldList>
+                    {/* <ChipField source="id" /> */}
+                    <ChipField source="name" />
+                </SingleFieldList>
+            </ArrayField>
         </Datagrid>
     </List>
 );
--- src/containers/AdminContainer.js
+++ src/containers/AdminContainer.js
@@ -1,68 +1,42 @@
 import React from 'react';
 import { connect, Provider } from 'react-redux';
 import { createHashHistory } from 'history';
-import { fetchUtils, Admin, Resource } from 'react-admin';
+import { fetchUtils, Admin, Resource, ListGuesser } from 'react-admin';
 import simpleRestProvider from 'ra-data-simple-rest';
 // import defaultMessages from 'ra-language-english';
 // import polyglotI18nProvider from 'ra-i18n-polyglot';
 // import messages from 'i18n';
 import createAdminStore from '../createAdminStore';
-import { stringify } from 'query-string';
-
+// import { stringify } from 'query-string';
 
 // your app components
 import { PostList } from '../adminComponents/posts';
 
-// const httpClient = (url, options={}) => {
-//     if (!options.headers) {
-//         options.headers = new Headers({
-//             'Content-Type': 'application/json',
-//             'Accept': 'application/json'
-//         });
+// const apiUrl = 'http://localhost:3000/api'
+// const httpClient = fetchUtils.fetchJson;
+
+// const dataProvider = {
+//     getList: (resource, params) => {
+//         const { page, perPage } = params.pagination;
+//         const { field, order } = params.sort;
+//         const query = {
+//             sort: JSON.stringify([field, order]),
+//             range: JSON.stringify([(page - 1) * perPage, page * perPage - 1]),
+//             filter: JSON.stringify(params.filter),
+//         };
+//         const url = `${apiUrl}/${resource}?${stringify(query)}`;
+
+//         return httpClient(url).then(({ headers, json }) => ({
+//             data: json,
+//             total: parseInt(headers.get('content-range').split('/').pop(), 10),
+//         }));
 //     }
-//     // add your own headers here
-//     // options.headers.set('X-Custom-Header', 'foobar');
-
-//     return fetchUtils.fetchJson(url, options)
-//     // .then(res => {
-//     //     console.log(res)
-//     //     // const headers = new Headers();
-        
-//     //     // console.log(headers)
-//     //     // headers.append( 'Access-Control-Expose-Headers', 'Content-Range');
-//     //     // headers.append('Content-Range','bytes : 0-9/9') // should return 'text/xml'
-
-//     //     // res.headers.set( 'Access-Control-Expose-Headers', 'Content-Range')
-//     //     // res.headers.set('Content-Range','bytes : 0-9/9')
-//     // });
-    
 // }
-const apiUrl = 'http://localhost:3000/api'
-const httpClient = fetchUtils.fetchJson;
-
-const dataProvider = {
-    getList: (resource, params) => {
-        const { page, perPage } = params.pagination;
-        const { field, order } = params.sort;
-        const query = {
-            sort: JSON.stringify([field, order]),
-            range: JSON.stringify([(page - 1) * perPage, page * perPage - 1]),
-            filter: JSON.stringify(params.filter),
-        };
-        const url = `${apiUrl}/${resource}?${stringify(query)}`;
-
-        return httpClient(url).then(({ headers, json }) => (
-            // console.log(headers.get('content-range')),
-            {
-            data: json,
-            total: parseInt(headers.get('content-range').split('/').pop(), 10),
-        }));
-    }
-}
 
 // dependency injection
-// dataProvider = simpleRestProvider('http://localhost:3000/api');
+const dataProvider = simpleRestProvider('http://localhost:3000/api');
 
+//customize dataProvider
 // dataProvider.getList('posts', {
 //     pagination: { page: 1, perPage: 5 },
 //     sort: { field: 'title', order: 'ASC' }
@@ -71,7 +45,6 @@ const dataProvider = {
 
 const history = createHashHistory();
 
-// const authProvider = () => Promise.resolve();
 // const i18nProvider = polyglotI18nProvider(locale => {
 //     if (locale !== 'en') {
 //         return messages[locale];
@@ -84,13 +57,11 @@ const AdminContainer = () => (
     //react admin requires it's own seperate redux store
     <Provider
         store={createAdminStore({
-            // authProvider,
             dataProvider,
             history,
         })}
     >
         <Admin
-            // authProvider={authProvider}
             dataProvider={dataProvider}
             history={history}
             title="My Admin"
