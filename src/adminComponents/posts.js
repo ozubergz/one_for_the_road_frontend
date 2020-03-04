f1fdcfbbf3ce835e997ff42a5cca9a82dcd04d05
ozubergz <jmin499@gmail.com>
2020-03-04 17:27:31 -0500

    update items

--- src/adminComponents/posts.js
+++ src/adminComponents/posts.js
@@ -1,18 +1,96 @@
 import React from 'react';
-import { List, Datagrid, TextField,  ArrayField, SingleFieldList, ChipField } from 'react-admin';
-// import { List, Datagrid, TextField, ArrayInput, SimpleFormIterator, DateInput, TextInput } from 'react-admin';
+import { 
+    Edit,
+    Show, 
+    List, 
+    SimpleForm,
+    Datagrid, 
+    TextField,
+    TextInput,  
+    NumberInput,
+    ArrayField, 
+    SimpleShowLayout,
+    SelectInput,
+    ReferenceInput, 
+    ReferenceField, 
+    NumberField,
+    EditButton,
+    ShowButton 
+ } from 'react-admin';
 
-
-export const PostList = (props) => (
+export const CategoryList = (props) => (
     <List {...props}>
         <Datagrid>
             <TextField source="id" />
             <TextField source="name" />
-            <ArrayField source="items">
-                <SingleFieldList linkType={false} >
-                    <ChipField source="name" />
-                </SingleFieldList>
-            </ArrayField>
+            <ShowButton />
+            <EditButton />
         </Datagrid>
     </List>
+);
+
+export const CategoryShow = props => (
+    <Show {...props}>
+        <SimpleShowLayout>
+            <TextField source="id" />
+            <TextField source="name" />
+            <ArrayField source="items">
+                <Datagrid>
+                    <TextField source="id" />
+                    <TextField source="name" />
+                    <TextField source="description" />
+                    <NumberField source="price" />
+                    <ReferenceField source="category_id" reference="categories">
+                        <TextField source="name" />
+                    </ReferenceField>
+                </Datagrid>
+            </ArrayField>
+        </SimpleShowLayout>
+    </Show>
+);
+
+
+export const ItemList = props => (
+    <List {...props}>
+        <Datagrid>
+            <TextField source="id" />
+            <TextField source="name" />
+            <TextField source="description" />
+            <NumberField source="price" />
+            <ReferenceField source="category_id" reference="categories">
+                <TextField source="name" />
+            </ReferenceField>
+            <ShowButton/>
+            <EditButton />
+        </Datagrid>
+    </List>
+);
+
+export const ItemShow = props => (
+    <Show {...props}>
+        <SimpleShowLayout>
+            <TextField source="id" />
+            <TextField source="name" />
+            <TextField source="description" />
+            <NumberField source="price" />
+            <ReferenceField source="category_id" reference="categories">
+                <TextField source="name" />
+            </ReferenceField>
+        </SimpleShowLayout>
+    </Show>
+);
+
+export const ItemEdit = props => (
+    <Edit {...props}>
+        <SimpleForm>
+            <TextInput disabled source="id" />
+            <TextInput source="name" />
+            <TextInput source="description" />
+            <NumberInput source="price" />
+            {/* <TextInput source="selections" /> */}
+            <ReferenceInput source="category_id" reference="categories">
+                <SelectInput optionText="name" />
+            </ReferenceInput>
+        </SimpleForm>
+    </Edit>
 );
--- src/containers/AdminContainer.js
+++ src/containers/AdminContainer.js
@@ -1,40 +1,44 @@
 import React from 'react';
-import { connect, Provider } from 'react-redux';
+import { Provider } from 'react-redux';
 import { createHashHistory } from 'history';
-import { fetchUtils, Admin, Resource, ListGuesser, ShowGuesser } from 'react-admin';
 import simpleRestProvider from 'ra-data-simple-rest';
+import createAdminStore from '../createAdminStore';
+import { 
+    Admin, 
+    Resource, 
+    ListGuesser, 
+    fetchUtils, 
+    ShowGuesser, 
+    EditGuesser
+ } from 'react-admin';
+
 // import defaultMessages from 'ra-language-english';
 // import polyglotI18nProvider from 'ra-i18n-polyglot';
 // import messages from 'i18n';
-import createAdminStore from '../createAdminStore';
 // import { stringify } from 'query-string';
 
 // your app components
-import { PostList } from '../adminComponents/posts';
-
-// const apiUrl = 'http://localhost:3000/api'
-// const httpClient = fetchUtils.fetchJson;
-
-// const dataProvider = {
-//     getList: (resource, params) => {
-//         const { page, perPage } = params.pagination;
-//         const { field, order } = params.sort;
-//         const query = {
-//             sort: JSON.stringify([field, order]),
-//             range: JSON.stringify([(page - 1) * perPage, page * perPage - 1]),
-//             filter: JSON.stringify(params.filter),
-//         };
-//         const url = `${apiUrl}/${resource}?${stringify(query)}`;
-
-//         return httpClient(url).then(({ headers, json }) => ({
-//             data: json,
-//             total: parseInt(headers.get('content-range').split('/').pop(), 10),
-//         }));
-//     }
-// }
+import { 
+    CategoryList, 
+    CategoryShow, 
+    ItemList, 
+    ItemShow,
+    ItemEdit
+ } from '../adminComponents/posts';
 
 // dependency injection
 const dataProvider = simpleRestProvider('http://localhost:3000/api');
+const history = createHashHistory();
+
+// const apiUrl = 'http://localhost:3000/api';
+// const httpClient = fetchUtils.fetchJson;
+
+// dataProvider.update((resource, params) => {
+//     httpClient(`${apiUrl}/${resource}/${params.id}`, {
+//         method: 'PUT',
+//         body: JSON.stringify(params.data),
+//     }).then(({ json }) => ({ data: json }))
+// });
 
 //customize dataProvider
 // dataProvider.getList('posts', {
@@ -43,7 +47,6 @@ const dataProvider = simpleRestProvider('http://localhost:3000/api');
 //     // filter: { author_id: 12 },
 // }).then(res => console.log(res))
 
-const history = createHashHistory();
 
 // const i18nProvider = polyglotI18nProvider(locale => {
 //     if (locale !== 'en') {
@@ -66,17 +69,22 @@ const AdminContainer = () => (
             history={history}
             title="My Admin"
         >
-            <Resource name="categories" list={PostList}/>
-            <Resource name="items" list={ListGuesser} />
+            <Resource 
+                name="categories" 
+                list={CategoryList} 
+                show={CategoryShow} 
+                edit={EditGuesser}
+            />
+
+            <Resource 
+                name="items" 
+                list={ItemList} 
+                show={ItemShow} 
+                edit={ItemEdit}
+            />
         </Admin>
     </Provider>
 );
 
-const mapStateToProps = state => {
-    return {
-        menus: state.menu
-        // cartItems: state.cart
-    }
-}
 
-export default connect(mapStateToProps)(AdminContainer);
+export default AdminContainer;
