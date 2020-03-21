0303e3a9be94bdd9c4c85fe5858a2234ea3dd74d
ozubergz <jmin499@gmail.com>
2020-03-21 16:48:10 -0400

    radio options toggle

--- src/containers/ItemOptionsContainer.js
+++ src/containers/ItemOptionsContainer.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 
 class ItemOptionsContainer extends Component {
 
-    renderOptions(options) {
+    renderOptions(itemOptionId, options) {
         return options.map(option => {
             return (
-                <div className="form-check">
-                    <input className="form-check-input" type="radio" name={option.name} id={option.id} />
+                <div className="form-check form-check-inline">
+                    <input className="form-check-input" type="radio" name={`option-${itemOptionId}`} value={option.name} id={option.id} />
                     <label className="form-check-label" htmlFor={option.id}>
                         {option.name}
                     </label>
@@ -22,7 +22,7 @@ class ItemOptionsContainer extends Component {
                     return(
                         <div key={itemOption.id} className="items-options" style={{display: this.props.displayOptions ? '' : 'none' }}>
                             <h6>{itemOption.name}</h6>
-                            {this.renderOptions(itemOption.options)}
+                            {this.renderOptions(itemOption.id, itemOption.options)}
                         </div>
                     )
                 })}
