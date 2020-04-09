735bf85850678e62a02a061e73e914a5aad91879
ozubergz <jmin499@gmail.com>
2020-04-09 19:52:35 -0400

    item options displays

--- src/containers/ItemOptionsContainer.js
+++ src/containers/ItemOptionsContainer.js
@@ -8,7 +8,7 @@ class ItemOptionsContainer extends Component {
                 <div key={option.id} className="form-check form-check-inline">
                     <input className="form-check-input" type={option.input_type} name={`option-${itemOptionId}`} value={option.id} id={option.id} />
                     <label className="form-check-label" htmlFor={option.id}>
-                        {option.name}
+                        {option.name} {option.price ? `$${option.price.toFixed(2)}` : null}
                     </label>
                 </div>
             )
@@ -20,12 +20,24 @@ class ItemOptionsContainer extends Component {
             <div>
                 {this.props.itemOptions.map(itemOption => {
                     return(
-                        <div key={itemOption.id} className="items-options" style={{display: this.props.displayOptions ? '' : 'none' }}>
+                        <div 
+                            key={itemOption.id} 
+                            className="items-options" 
+                            style={{display: this.props.displayOptions ? '' : 'none' }}
+                        >
                             <h6>{itemOption.name}</h6>
                             {this.renderOptions(itemOption.id, itemOption.options)}
                         </div>
                     )
                 })}
+                
+                { this.props.displayOptions ?
+                    <div className="option-btn-group">
+                        <button className="option-add-btn btn btn-danger btn-sm" >Add to Cart</button> 
+                    </div>
+                        : 
+                    null
+                } 
             </div>
         );
     }
