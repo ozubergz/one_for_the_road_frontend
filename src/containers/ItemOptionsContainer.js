716ec16a042b117ac258b65907d2a9b14d87a2e1
ozubergz <jmin499@gmail.com>
2020-04-09 21:56:02 -0400

    styled item options

--- src/containers/ItemOptionsContainer.js
+++ src/containers/ItemOptionsContainer.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 
 class ItemOptionsContainer extends Component {
 
-    renderOptions(itemOptionId, options) {
+    renderInputOptions(itemOptionId, options) {
         return options.map(option => {
             return (
                 <div key={option.id} className="form-check form-check-inline">
                     <input className="form-check-input" type={option.input_type} name={`option-${itemOptionId}`} value={option.id} id={option.id} />
                     <label className="form-check-label" htmlFor={option.id}>
-                        {option.name} {option.price ? `$${option.price.toFixed(2)}` : null}
+                        {option.name} {option.price && option.price !== 0 ? `$${option.price.toFixed(2)}` : null}
                     </label>
                 </div>
             )
@@ -25,8 +25,20 @@ class ItemOptionsContainer extends Component {
                             className="items-options" 
                             style={{display: this.props.displayOptions ? '' : 'none' }}
                         >
-                            <h6>{itemOption.name}</h6>
-                            {this.renderOptions(itemOption.id, itemOption.options)}
+                            <div className="item-options-title">
+                                <h6>{itemOption.name}</h6>
+                            </div>
+                            <div className="item-optitons-required">
+                                { 
+                                    itemOption.required === 'true' ?
+                                        <span style={{color: "red"}}>Required - Choose 1</span>
+                                            :
+                                        <span style={{color: "grey"}}>Optional</span>
+                                }
+                            </div>
+                            <div className="input-option-group">
+                                {this.renderInputOptions(itemOption.id, itemOption.options)}
+                            </div>
                         </div>
                     )
                 })}
