e064387abbcb5ad70bc15f8a56e19cb2da78fe6a
ozubergz <jmin499@gmail.com>
2020-05-03 19:21:21 -0400

    add contact number at homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -15,7 +15,9 @@ class Home extends Component {
                     <div className="section-1">
                         <div className="section-1-body">
                             <img className="section-1-logo" src={logo} alt="One for the Road Logo" />
+                            
                             <div className="section-1-content">
+
                                 <div className="schedule">
                                     <h5 className="schedule-title">
                                         <i className="fa fa-clock-o"></i> 
@@ -42,20 +44,31 @@ class Home extends Component {
                                         </div>
                                     </div>
                                 </div>
-                                <div className="address-container">
-                                    <h5 className="address-title">
-                                        <i className="fa fa-home"></i>
-                                        Address
-                                    </h5>
-                                    <div className="address">
-                                        <div>
-                                            40-25 150th Street
-                                        </div>
-                                        <div>
-                                            Flushing, NY 11354
+
+                                <div className="address-contact">
+                                    <div className="address-container">
+                                        <h5 className="address-title">
+                                            <i className="fa fa-home"></i>
+                                            Address
+                                        </h5>
+                                        <div className="address">
+                                            <div>
+                                                40-25 150th Street
+                                            </div>
+                                            <div>
+                                                Flushing, NY 11354
+                                            </div>
                                         </div>
                                     </div>
+                                    <div className="contact-container">
+                                        <h5 className="contact-title"> 
+                                            <i className="fa fa-phone"></i>
+                                            Contact
+                                        </h5>
+                                        <h6>347-732-9732</h6>
+                                    </div>
                                 </div>
+
                             </div>
                         </div>
                     </div>
