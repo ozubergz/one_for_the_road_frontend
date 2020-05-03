0b3dc3d6cc10df9b74900ec388facd8b969d7da1
ozubergz <jmin499@gmail.com>
2020-05-03 17:31:27 -0400

    redesign schedule

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,28 +14,33 @@ class Home extends Component {
 
                     <div className="section-1">
                         <img className="home-page-logo" src={logo} alt="One for the Road Logo" />
-
                         <div className="schedule">
-                            {/* <h5 className="footer-heading"><i className="fa fa-clock-o"></i> Business Hours</h5> */}
-                            <div className="days">
-                                <div>Mon</div>
-                                <div>Tues</div>
-                                <div>Wed</div>
-                                <div>Thu</div>
-                                <div>Fri</div>
-                                <div>Sat</div>
-                                <div>Sun</div>
-                            </div>
-                            <div className="time">
-                                <div>6:30am - 9:00pm</div>
-                                <div>6:30am - 9:00pm</div>
-                                <div>6:30am - 9:00pm</div>
-                                <div>6:30am - 9:00pm</div>
-                                <div>6:30am - 9:00pm</div>
-                                <div>6:30am - 9:00pm</div>
-                                <div>6:30am - 9:00pm</div>
+                            <h5 className="schedule-title">
+                                <i className="fa fa-clock-o"></i> 
+                                Business Hours
+                            </h5>
+                            <div className="schedule-time-day">
+                                <div className="days">
+                                    <div>Mon</div>
+                                    <div>Tues</div>
+                                    <div>Wed</div>
+                                    <div>Thu</div>
+                                    <div>Fri</div>
+                                    <div>Sat</div>
+                                    <div>Sun</div>
+                                </div>
+                                <div className="time">
+                                    <div>6:30am - 9:00pm</div>
+                                    <div>6:30am - 9:00pm</div>
+                                    <div>6:30am - 9:00pm</div>
+                                    <div>6:30am - 9:00pm</div>
+                                    <div>6:30am - 9:00pm</div>
+                                    <div>6:30am - 9:00pm</div>
+                                    <div>6:30am - 9:00pm</div>
+                                </div>
                             </div>
                         </div>
+
 
                     </div>
                 </div>
