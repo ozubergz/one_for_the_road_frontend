f4f1a73591c3efce52dbe190440000cb0fd1fbcf
ozubergz <jmin499@gmail.com>
2020-05-03 18:32:55 -0400

    redesigned section-1 at homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,52 +13,54 @@ class Home extends Component {
                 <div className="home-body">
 
                     <div className="section-1">
-                        <img className="home-page-logo" src={logo} alt="One for the Road Logo" />
-                        <div className="schedule">
-                            <h5 className="schedule-title">
-                                <i className="fa fa-clock-o"></i> 
-                                Business Hours
-                            </h5>
-                            <div className="schedule-time-day">
-                                <div className="days">
-                                    <div>Mon</div>
-                                    <div>Tues</div>
-                                    <div>Wed</div>
-                                    <div>Thu</div>
-                                    <div>Fri</div>
-                                    <div>Sat</div>
-                                    <div>Sun</div>
+                        <div className="section-1-body">
+                            <img className="section-1-logo" src={logo} alt="One for the Road Logo" />
+                            <div className="section-1-content">
+                                <div className="schedule">
+                                    <h5 className="schedule-title">
+                                        <i className="fa fa-clock-o"></i> 
+                                        Business Hours
+                                    </h5>
+                                    <div className="schedule-time-day">
+                                        <div className="days">
+                                            <div>Mon</div>
+                                            <div>Tues</div>
+                                            <div>Wed</div>
+                                            <div>Thu</div>
+                                            <div>Fri</div>
+                                            <div>Sat</div>
+                                            <div>Sun</div>
+                                        </div>
+                                        <div className="time">
+                                            <div>6:30am - 9:00pm</div>
+                                            <div>6:30am - 9:00pm</div>
+                                            <div>6:30am - 9:00pm</div>
+                                            <div>6:30am - 9:00pm</div>
+                                            <div>6:30am - 9:00pm</div>
+                                            <div>6:30am - 9:00pm</div>
+                                            <div>6:30am - 9:00pm</div>
+                                        </div>
+                                    </div>
                                 </div>
-                                <div className="time">
-                                    <div>6:30am - 9:00pm</div>
-                                    <div>6:30am - 9:00pm</div>
-                                    <div>6:30am - 9:00pm</div>
-                                    <div>6:30am - 9:00pm</div>
-                                    <div>6:30am - 9:00pm</div>
-                                    <div>6:30am - 9:00pm</div>
-                                    <div>6:30am - 9:00pm</div>
+                                <div className="address-container">
+                                    <h5 className="address-title">
+                                        <i className="fa fa-home"></i>
+                                        Address
+                                    </h5>
+                                    <div className="address">
+                                        <div>
+                                            40-25 150th Street
+                                        </div>
+                                        <div>
+                                            Flushing, NY 11354
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-
-                        <div className="address-container">
-                            <h5 className="address-title">
-                                <i className="fa fa-home"></i>
-                                Address
-                            </h5>
-                            <div className="address">
-                                <div>
-                                    40-25 150th Street
-                                </div>
-                                <div>
-                                    Flushing, NY 11354
-                                </div>
-                            </div>
-                        </div>
-
                     </div>
-                </div>
 
+                </div>
             </div>
         );
     }
